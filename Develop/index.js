1235e0efe1f5a296673a3f45c8d8383d3ec566eb
Celine Davilmar <rdavi106@fiu.edu>
2022-05-18 14:26:00 -0400

    imported express

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,16 @@
 // TODO: Include packages needed for this application
+import express from "Express";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your project title?", "Please enter description, installation instructions, usage information, contribution guidelines, and test instructions "
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
