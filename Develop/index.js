9719e6ccb7b79ba84cbccdb245fbf7a522a72a9a
Celine Davilmar <rdavi106@fiu.edu>
2022-05-22 19:27:27 -0400

    updated

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,78 @@
 // TODO: Include packages needed for this application
-import express from "Express";
+const inquierer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is your project title?", "Please enter description, installation instructions, usage information, contribution guidelines, and test instructions "
+    {
+        type: 'input',
+        message: "What is your project title?",
+        name: 'projectTitle',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid Project Title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Please enter description",
+        name: 'description',
+        default: 'description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A Discription is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Please enter the installation instructions",
+        name: 'instructions',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid response is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Please enter the contribution guidelines",
+        name: 'guidlines',
+        default: 'guidlines',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A Discription is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Please enter the test instructions.",
+        name: 'test',
+        default: 'test',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A Discription is required.");
+            }
+            return true;
+        }
+    },
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+
+}
 
 // Function call to initialize app
 init();
